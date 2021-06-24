63524ffa13e4f1caa579bfd838d498324e990c83
Bluzzi <camilledugas.pro@gmail.com>
2021-06-24 14:55:50 +0200

    Create __error.svelte

---
+++ src/routes/__error.svelte
@@ -0,0 +1,18 @@
+<script context="module">
+	export function load({error, status}){
+		return {
+			props: {
+                errorCode: status,
+				message: error.message.toLowerCase()
+			}
+		};
+	}
+</script>
+
+<script>
+	export let errorCode;
+    export let message;
+</script>
+
+<p>{errorCode}</p>
+<p>{message}</p>
