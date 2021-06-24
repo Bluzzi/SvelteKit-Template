6615e035176679b6c314e42f0826c58511ce4651
Bluzzi <camilledugas.pro@gmail.com>
2021-06-24 16:30:03 +0200

    Create index and components pages

---
+++ src/routes/components.svelte
@@ -0,0 +1,34 @@
+<script>
+    import Button from "$lib/components/elements/Button.svelte";
+    import Clock from "$lib/components/elements/Clock.svelte";
+</script>
+
+<svelte:head>
+    <title>Svelte - Components</title>
+</svelte:head>
+
+<div class="content">
+    <p>Some components of this template :</p>
+
+    <Clock/>
+
+    <div class="buttons">
+        <Button href="/" external={false} hoverStyle="void" style="margin-right: 10px;">Home</Button>
+        <Button href="https://svelte.dev/" external={true} hoverStyle="brightness" style="margin-right: 10px;">Svelte</Button> 
+    </div>
+</div>
+
+<style>
+    .content {
+        display: flex;
+        flex-direction: column;
+
+        .buttons {
+            display: flex;
+
+            * {
+                margin: $space-1 $space-5;
+            }
+        }
+    }
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,23 @@
+<script>
+    import Button from "$lib/components/elements/Button.svelte";
+    import Clock from "$lib/components/elements/Clock.svelte";
+</script>
+
+<svelte:head>
+    <title>Svelte - Home</title>
+</svelte:head>
+
+<div class="svelte">
+    <p>Documentation Svelte :</p>
+
+    <div class="buttons">
+        <Button href="https://svelte.dev/" external={true} hoverStyle="brightness" style="margin-right: 10px;">Svelte</Button>
+        <Button href="https://kit.svelte.dev/" external={true} hoverStyle="brightness" style="margin-right: 10px;">Svelte Kit</Button>
+    </div>
+</div>
+
+<style>
+    .buttons {
+        display: flex;
+    }
+</style>
