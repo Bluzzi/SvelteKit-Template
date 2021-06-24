5529aec5678066e4d6b955563adfb127bb5616d7
Bluzzi <camilledugas.pro@gmail.com>
2021-06-24 15:30:44 +0200

    Create Clock component

---
+++ src/lib/components/Clock.svelte
@@ -0,0 +1,20 @@
+<script>
+    import dayjs from "dayjs";
+
+    let currentTime = "";
+
+    let timeUpdate = () => {
+        currentTime = dayjs().format("H [hours,] m [minutes,] s [seconds]");
+    }
+
+    timeUpdate();
+    setInterval(timeUpdate, 1000);
+</script>
+
+<p>{currentTime}</p>
+
+<style>
+    p {
+        font-size: $font-size-p;
+    }
+</style>
